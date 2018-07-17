605f0f4e4df96e7245104401756102a5ffdc88b6
Wardormeur <wardormeur@gmail.com>
2018-07-17 14:47:52 +0100

    Add force to reinstall post restart of the image

--- wp-init.sh
+++ wp-init.sh
@@ -24,21 +24,21 @@ then
   wp plugin delete akismet --allow-root
 fi
 
-wp plugin install timber-library --version=1.3.3 --activate --allow-root
-wp plugin install pods --version=2.6.11 --activate --allow-root
-wp plugin install antispam-bee --version=2.7.1 --activate --allow-root
-wp plugin install caldera-forms --version=1.5.2.1 --activate --allow-root
-wp plugin install contact-form-7 --version=4.8 --activate --allow-root
-wp plugin install custom-share-buttons-with-floating-sidebar --version=3.3 --activate --allow-root
-wp plugin install google-captcha --version=1.33 --activate --allow-root
-wp plugin install google-sitemap-generator --version=4.0.9 --activate --allow-root
-wp plugin install wonderm00ns-simple-facebook-open-graph-tags --version=2.2.4.1 --activate --allow-root
-wp plugin install redirection --version=2.8 --activate --allow-root
-wp plugin install tablepress --version=1.8.1 --activate --allow-root
-wp plugin install wp-mail-smtp --version=0.10.1 --activate --allow-root
-wp plugin install wp-super-cache --version=1.6.2 --activate --allow-root
-wp plugin install sucuri-scanner --version=1.8.11 --activate --allow-root
-wp plugin install https://github.com/humanmade/S3-Uploads/archive/f9f09b1ead9e07032ee1eb406a237b1273fe55ed.zip --activate --allow-root
+wp plugin install timber-library --force --version=1.3.3 --activate --allow-root
+wp plugin install pods --force --version=2.6.11 --activate --allow-root
+wp plugin install antispam-bee --force --version=2.7.1 --activate --allow-root
+wp plugin install caldera-forms --force --version=1.5.2.1 --activate --allow-root
+wp plugin install contact-form-7 --force --version=4.8 --activate --allow-root
+wp plugin install custom-share-buttons-with-floating-sidebar --force --version=3.3 --activate --allow-root
+wp plugin install google-captcha --force --version=1.33 --activate --allow-root
+wp plugin install google-sitemap-generator --force --version=4.0.9 --activate --allow-root
+wp plugin install wonderm00ns-simple-facebook-open-graph-tags --force --version=2.2.4.1 --activate --allow-root
+wp plugin install redirection --force --version=2.8 --activate --allow-root
+wp plugin install tablepress --force --version=1.8.1 --activate --allow-root
+wp plugin install wp-mail-smtp --force --version=0.10.1 --activate --allow-root
+wp plugin install wp-super-cache --force --version=1.6.2 --activate --allow-root
+wp plugin install sucuri-scanner --force --version=1.8.11 --activate --allow-root
+wp plugin install https://github.com/humanmade/S3-Uploads/archive/f9f09b1ead9e07032ee1eb406a237b1273fe55ed.zip --force --activate --allow-root
 
 wp theme activate cd-theme --allow-root
 exec "$@"
